f49cb0b19ac87dad1d5aa437471984747d34c739
mona <1056047818@qq.com>
2020-02-29 19:45:04 +0800

    'feat:树'

---
+++ 树/inOrderTreeTraverse.js
@@ -0,0 +1,48 @@
+// 中序遍历
+function BinarySearchTree() {
+  var Node = function(key) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  };
+  var root = null;
+
+  this.insert = function(key) {
+    var newNode = new Node(key);
+
+    if (root === null) {
+      root = newNode;
+    } else {
+      insertNode(root, newNode);
+    }
+  console.log(root);
+
+  };
+
+  // keys && keys.forEach(key => {
+  //   this.insert(key);
+  // });
+
+  function insertNode(node, newNode) {
+    if (newNode.key < node.key) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        insertNode(node.right, newNode);
+      }
+    }
+  }
+  console.log(root);
+  
+  return root;
+}
+
+var tree = new BinarySearchTree();
+tree.insert(11);
+console.log(tree);
