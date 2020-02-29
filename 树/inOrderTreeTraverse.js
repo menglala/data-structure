669db6a3ed73b5667e293e382438072fdabb4f06
mona <1056047818@qq.com>
2020-02-29 20:32:11 +0800

    ..

--- 树/inOrderTreeTraverse.js
+++ 树/inOrderTreeTraverse.js
@@ -15,8 +15,7 @@ function BinarySearchTree() {
     } else {
       insertNode(root, newNode);
     }
-  console.log(root);
-
+    console.log(root);
   };
 
   // keys && keys.forEach(key => {
@@ -39,10 +38,77 @@ function BinarySearchTree() {
     }
   }
   console.log(root);
-  
+
   return root;
 }
 
 var tree = new BinarySearchTree();
-tree.insert(11);
-console.log(tree);
+
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+  var sortHeights = [...heights];
+  sortHeights.sort((a, b) => {
+    return a - b;
+  });
+
+  var result = 0;
+  for (let i = 0; i < heights.length; i++) {
+    if (sortHeights[i] !== heights[i]) result++;
+  }
+
+  return result;
+};
+
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortedSquares = function(A) {
+  const sqrtA = A.map(n => Math.pow(n, 2)).sort((a, b) => a - b);
+  return sqrtA;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+  const sortNums = nums.sort((a, b) => a - b);
+
+  let sum = 0;
+  for (let i = 0; i < sortNums.length / 2; i++) {
+    sum += Math.min(sortNums[2 * i], sortNums[2 * i + 1]);
+  }
+
+  return sum;
+};
+var arrayPairSum2 = function(nums) {
+  return nums
+    .sort((a, b) => a - b)
+    .filter((item, index) => index % 2 === 0)
+    .reduce((t, i) => t + i);
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMagicIndex = function(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (i === nums[i]) {
+      return i;
+    }
+  }
+
+  return -1
+};
+console.log(heightChecker([1, 1, 4, 2, 1, 3]));
+console.log(sortedSquares([-4, -1, 0, 3, 10]));
+console.log(sortedSquares([-7, -3, 2, 3, 11]));
+console.log(arrayPairSum([1, 4, 3, 2]));
+console.log(arrayPairSum2([1, 4, 3, 2]));
+console.log(findMagicIndex([0, 2, 3, 4, 5]));
+console.log(findMagicIndex([1, 1, 1]));
